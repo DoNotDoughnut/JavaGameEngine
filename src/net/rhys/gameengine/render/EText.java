e6c7cb95430790fb2388caec5cd9e66cdc39a7f0
Rhys Holloway <rhysholloway@pacbell.net>
2020-01-17 17:57:51 -0800

    removed font from engine

--- src/net/rhys/gameengine/render/EText.java
+++ src/net/rhys/gameengine/render/EText.java
@@ -1,4 +1,4 @@
-package net.rhys.gameengine.render.text;
+package net.rhys.gameengine.render;
 
 import java.awt.Color;
 import java.awt.Font;
@@ -41,6 +41,7 @@ public class EText{
 	public static void render(Graphics graphics) {
 		
 		Graphics2D g = (Graphics2D) graphics;
+		
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		
 		for(EText text : texts)
